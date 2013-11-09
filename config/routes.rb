00be9a95a48a88630c4b76844505619080f0c4a1
Pattie Reaves <pattie@pzunk.net>
2013-11-08 21:20:38 -0500

    adds routes for main page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Bourbon::Application.routes.draw do
+  # new 
+  # created
+  # edit
+  # update
+  # index
+  # show
+  # destroy
+
+  resources :bourbon_whiskeys, only: [:index]
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
