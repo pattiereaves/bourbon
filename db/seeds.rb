d1f458f580b01ba94ddca53970575cf4bd77152f
Pattie Reaves <pattie@pzunk.net>
2013-11-09 14:15:29 -0500

    first draft of csv parser to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,51 +4,82 @@
 # Delete stuff
 Distillery.delete_all
 BourbonWhiskey.delete_all
-Flavor.delete_all
-
-# Make stuff
-sweet = Flavor.create(name: "sweet")
-fruity = Flavor.create(name: "fruity")
-mild = Flavor.create(name: "mild")
+AttributeType.delete_all
+BourbonAttribute.delete_all
+BourbonProfile.delete_all
 
 
-jim = Distillery.create(name: "Jim Beam")
-  jim.bourbon_whiskeys.create(name: "Jim Beam Bourbon", flavors: [mild])
-  jim.bourbon_whiskeys.create(name: "Jim Beam Black Label 8 Year Old Bourbon Whiskey", flavors: [mild])
+# Add attributes and types
+AttributeTypeArray = ['taste','aroma','finish']
+BourbonAttributeArray = ['wood','fruit','sweet','spice','grain']
 
-woodford = Distillery.create(name: "Woodford Reserve")
-  woodford.bourbon_whiskeys.create(name: "Woodford Reserve Bourbon", flavors: [fruity])
-  woodford.bourbon_whiskeys.create(name: "Woodford Reserve Double Oaked Bourbon", flavors: [mild])
-
-
-buffalo = Distillery.create(name: "Buffalo Trace")
-  buffalo.bourbon_whiskeys.create(name: "Buffalo Trace Bourbon", flavors: [sweet])
-  buffalo.bourbon_whiskeys.create(name: "Buffalo Trace Experimental Collection Rice Mash Bourbon", flavors: [mild])
-  buffalo.bourbon_whiskeys.create(name: "Buffalo Trace Experimental Collection Oat Mash Bourbon", flavors: [fruity])
-
-makers = Distillery.create(name: "Maker's Mark")
-  makers.bourbon_whiskeys.create(name: "Maker's Mark", flavors: [fruity])
-  makers.bourbon_whiskeys.create(name: "Maker's 46", flavors: [mild])
+AttributeTypeArray.each do |type|
+  attribute_type = AttributeType.create(name: type)
+  BourbonAttributeArray.each do |btype|
+    attribute_type.bourbon_attributes.create(name: btype, attribute_type_id: attribute_type.id)
+  end
+end
+puts "Attribute Type and BourbonAttributes added!"
 
 
-roses = Distillery.create(name: "Four Roses")
-  roses.bourbon_whiskeys.create(name: "Four Roses Single Barrel", flavors: [fruity])
-  roses.bourbon_whiskeys.create(name: "Four Roses Small Batch Bourbon", flavors: [fruity])
-  roses.bourbon_whiskeys.create(name: "Four Roses Bourbon Yellow Label", flavors: [sweet])
+
+require 'csv'
+
+  csv_file_path = 'db/bourbons.csv'
+  CSV.foreach(csv_file_path,{:headers=>:first_row}) do |row|
+
+      distillery = Distillery.find_by_name(row[1])
+      unless distillery
+        distillery = Distillery.create({
+          name: row[1]  
+        })
+        puts "Distillery " + row[1] + " added!"
+      end
+
+      bourbon_whiskey = distillery.bourbon_whiskeys.create({
+        name: row[0],
+        age: row[2],
+        proof: row[3],
+        appearance: row[4],
+        aroma_description: row[5],
+        taste_description: row[7],
+        finish_description: row[9],
+      })
+
+      puts "BourbonWhiskey " +  row[0] + " added!"
 
 
-turkey = Distillery.create(name: "Wild Turkey")
-  turkey.bourbon_whiskeys.create(name: "Wild Turkey 101 Proof Bourbon", flavors: [sweet])
-  turkey.bourbon_whiskeys.create(name: "Wild Turkey Russell's Reserve Bourbon", flavors: [sweet])
-  turkey.bourbon_whiskeys.create(name: "Wild Turkey Rare Breed Bourbon", flavors: [mild])
-  turkey.bourbon_whiskeys.create(name: "Wild Turkey Tradition 14 Year Old Bourbon", flavors: [fruity])
+      bourbon_aromas = row[6].split(',')
+      bourbon_aromas.each do |val|
+        val.strip!
+        type_aroma = AttributeType.find_by_name("aroma");
+        b_attributes = BourbonAttribute.where("name = ? AND attribute_type_id = ?", val, type_aroma.id).limit(1)
+        bourbon_whiskey.bourbon_profiles.create({bourbon_attribute_id: b_attributes})
+      end
+      puts "Aroma profiles " +  row[6] + " added!"
 
 
-heaven = Distillery.create(name: "Heaven Hill")
-  heaven.bourbon_whiskeys.create(name: "Heaven Hill Gold Label Bonded Bourbon", flavors: [mild])
+      bourbon_tastes = row[8].split(',')
+      bourbon_tastes.each do |val|
+        val.strip!
+        type_taste = AttributeType.find_by_name("taste");
+        b_attributes = BourbonAttribute.where("name = ? AND attribute_type_id = ?", val, type_taste.id).limit(1)
+        bourbon_whiskey.bourbon_profiles.create({bourbon_attribute_id: b_attributes})
+      end
+      puts "Taste profiles " +  row[8] + " added!"
 
 
-barton = Distillery.create(name: "Barton 1792")
-  barton.bourbon_whiskeys.create(name: "Very Old Barton 86 Proof", flavors: [sweet])
+      bourbon_finishes = row[10].split(',')
+      bourbon_finishes.each do |val|
+        val.strip!
+        type_finish = AttributeType.find_by_name("finish");
+        b_attributes = BourbonAttribute.where("name = ? AND attribute_type_id = ?", val, type_finish.id).limit(1)
+        bourbon_whiskey.bourbon_profiles.create({bourbon_attribute_id: b_attributes})
+      end
+
+      puts "Finish profiles " +  row[10] + " added!"
+
+  end
+
 
 puts "Seeding finished"
