6e7ee522ab04b9f4ebe800183246321365917858
ebinion <ezekiel@gridshiftdesign.com>
2013-11-09 00:09:44 -0500

    Added db object to the index controller

--- app/controllers/bourbon_whiskeys_controller.rb
+++ app/controllers/bourbon_whiskeys_controller.rb
@@ -1,4 +1,10 @@
 class BourbonWhiskeysController < ApplicationController
+  # TODO: Sessions will have hated
+  # TODO: Sessions will have disliked
+  def index
+    @bourbon = BourbonWhiskey.random
+  end
+
 	def edit
     
     redirect_to root_url
